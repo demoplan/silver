2018ea1e603d6dd5c21d47368582a0879564c27c
Rakesh <er.rakesh2009@gmail.com>
2017-10-28 15:11:38 +0530

    new model created

---
+++ Silver Stock Management/StockManagement/DAL/Model/ReceiptModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Drawing;
+
+namespace StockManagement.DAL.Model
+{
+    public class ReceiptModel
+    {
+        // Stock Info        
+        public int TID { get; set; }
+
+        
+        public int SeqNo { get; set; }
+
+        
+        public Nullable<System.DateTime> TDate { get; set; }
+
+        
+        public string PCode { get; set; }
+
+        
+        public string BarCode { get; set; }
+
+        
+        public string MetalType { get; set; }
+
+        
+        public string InType { get; set; }
+
+        
+        public Nullable<int> RefVNo { get; set; }
+
+        
+        public Nullable<int> JobNo { get; set; }
+
+        
+        public Nullable<int> OrderNo { get; set; }
+
+        
+        public Nullable<decimal> Pcs { get; set; }
+
+        
+        public Nullable<decimal> GrossWt { get; set; }
+
+        
+        public Nullable<decimal> NetWt { get; set; }
+
+        
+        public Nullable<decimal> MakingRate { get; set; }
+
+        
+        public Nullable<decimal> TotalRate { get; set; }
+
+        
+        public Nullable<decimal> SellingRate { get; set; }
+
+        
+        public string Photo { get; set; }
+
+        public Image ProdImage {
+            //get {
+            //    if (File.Exists(Photo))
+            //    {
+            //        return Image.FromFile(Photo);
+            //    }
+            //    else
+            //        return null;
+            //}
+            //set
+            //{
+            //    ProdImage = value;
+            //}
+            get;set;
+        }
+
+        public Nullable<System.DateTime> OutDate { get; set; }
+        public Nullable<int> OutBillNo { get; set; }
+        public string OutType { get; set; }
+
+        //InOut Info  
+    }
+}
